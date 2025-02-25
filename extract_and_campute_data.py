d60931a84b54c68adb3c82470372c5677612de0a
GEEKGEEKOP <geekgeekop@yahoo.com>
2025-02-25 18:41:24 +0330

    clean data

--- extract_and_campute_data.py
+++ extract_and_campute_data.py
@@ -5,10 +5,10 @@ with open("messages.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 # فیلتر کردن داده‌ها: نگه داشتن فقط آنهایی که کلید "text" دارای مقدار معتبر است
-cleaned_data = [item for item in data if isinstance(item.get("text"), str) and item.get("text").strip()]
+cleaned_data = [item for item in data if isinstance(item.get("sender"), str) and item.get("sender").strip()]
 
 # ذخیره داده‌های تمیز شده در یک فایل جدید
-with open("cleaned_messages.json", "w", encoding="utf-8") as f:
+with open("cleaned_messages_2.json", "w", encoding="utf-8") as f:
     json.dump(cleaned_data, f, ensure_ascii=False, indent=4)
 
 print(f"تمیزسازی انجام شد. تعداد کل داده‌ها: {len(data)}. تعداد داده‌های باقی‌مانده: {len(cleaned_data)}.")
